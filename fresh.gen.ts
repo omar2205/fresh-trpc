7db8f8ea73b50cbc14f51307f9f69ff3057e88c3
omar2205 <omar2205a@yahoo.com>
2022-07-03 03:54:52 +0000

    add empty trpc route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,8 @@ import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_middleware.ts";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/posts.ts";
-import * as $4 from "./routes/index.tsx";
+import * as $4 from "./routes/api/trpc.ts";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,7 +16,8 @@ const manifest = {
     "./routes/_middleware.ts": $1,
     "./routes/api/joke.ts": $2,
     "./routes/api/posts.ts": $3,
-    "./routes/index.tsx": $4,
+    "./routes/api/trpc.ts": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
