0f80236aaa821b773c10b374e50e13fdd79c0e43
omar2205 <omar2205a@yahoo.com>
2022-07-03 02:41:36 +0000

    actually use the env var

--- utils/trpcClient.ts
+++ utils/trpcClient.ts
@@ -1,6 +1,8 @@
 import { createTRPCClient } from 'https://esm.sh/@trpc/client@10.0.0-alpha.30'
 import type { TRPCRouter } from './trpcRouter.ts'
 
+const API_URL = Deno.env.get('API_URL')
+
 export const client = createTRPCClient<TRPCRouter>({
-  url: 'https://comfortable-sheep-15.deno.dev/api/trpc',
+  url: API_URL ? API_URL : 'http://localhost:8000/api/trpc',
 })
