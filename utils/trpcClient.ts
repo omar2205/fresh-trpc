80c350d5fe6208cb79605e7bff66813a658a0680
omar2205 <omar2205a@yahoo.com>
2022-07-03 02:38:16 +0000

    use env for api_url

--- utils/trpcClient.ts
+++ utils/trpcClient.ts
@@ -2,5 +2,5 @@ import { createTRPCClient } from 'https://esm.sh/@trpc/client@10.0.0-alpha.30'
 import type { TRPCRouter } from './trpcRouter.ts'
 
 export const client = createTRPCClient<TRPCRouter>({
-  url: 'http://localhost:8000/api/trpc',
+  url: 'https://comfortable-sheep-15.deno.dev/api/trpc',
 })
